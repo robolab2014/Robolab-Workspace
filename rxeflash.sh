d8b83edfc9377361374534911eaf758bd258e92a
obihornchen <mirco.kuehnel@mailbox.tu-dresden.de>
2014-03-04 11:48:21 +0100

    neu

--- rxeflash.sh
+++
@@ -1,4 +0,0 @@
-echo Executing NeXTTool to upload main.rxe...
- /Applications/robolab/nxtOSEK/bin/nexttool /COM=usb -download=main.rxe
- /Applications/robolab/nxtOSEK/bin/nexttool /COM=usb -listfiles=main.rxe
-echo NeXTTool is terminated.
