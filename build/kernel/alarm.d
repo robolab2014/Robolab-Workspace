3750d64b4e695e5d79411edaa8310ca284147d84
obihornchen <mirco.kuehnel@mailbox.tu-dresden.de>
2014-03-04 12:10:55 +0100

    newtry

---
+++ build/kernel/alarm.d
@@ -0,0 +1,16 @@
+build/kernel/alarm.o: \
+ /Applications/robolab/nxtOSEK/ecrobot/../toppers_osek/kernel/alarm.c \
+ /Applications/robolab/nxtOSEK/ecrobot/../toppers_osek/kernel/osek_kernel.h \
+ /Applications/robolab/nxtOSEK/ecrobot/../toppers_osek/include/kernel.h \
+ /Applications/robolab/nxtOSEK/ecrobot/../toppers_osek/config/at91sam7s-gnu/lego_nxt/sys_defs.h \
+ /Applications/robolab/nxtOSEK/ecrobot/../toppers_osek/config/at91sam7s-gnu/cpu_defs.h \
+ /Applications/robolab/nxtOSEK/ecrobot/../toppers_osek/include/osek.h \
+ /Applications/robolab/nxtOSEK/ecrobot/../toppers_osek/include/t_stddef.h \
+ /Applications/robolab/nxtOSEK/ecrobot/../toppers_osek/config/at91sam7s-gnu/tool_defs.h \
+ /Applications/robolab/nxtOSEK/ecrobot/../toppers_osek/include/t_config.h \
+ /Applications/robolab/nxtOSEK/ecrobot/../toppers_osek/config/at91sam7s-gnu/lego_nxt/sys_config.h \
+ /Applications/robolab/nxtOSEK/ecrobot/../toppers_osek/config/at91sam7s-gnu/cpu_config.h \
+ /Applications/robolab/nxtOSEK/ecrobot/../toppers_osek/config/at91sam7s-gnu/cpu_insn.h \
+ /Applications/robolab/nxtOSEK/ecrobot/../toppers_osek/config/at91sam7s-gnu/tool_config.h \
+ /Applications/robolab/nxtOSEK/ecrobot/../toppers_osek/kernel/check.h \
+ /Applications/robolab/nxtOSEK/ecrobot/../toppers_osek/kernel/alarm.h
---
+++ kernel_id.h
@@ -0,0 +1,32 @@
+/*
+ *  kernel_id.h
+ *  Tue Mar 04 11:58:51 2014
+ *  SG Version 2.00
+ *  sg ./main.oil -os=ECC2 -I/Applications/robolab/nxtOSEK/toppers_osek/sg/impl_oil -template=/Applications/robolab/nxtOSEK/toppers_osek/sg/lego_nxt.sgt
+ */
+#ifndef KERNEL_ID_H
+#define KERNEL_ID_H
+
+ /****** Object OS ******/
+
+#define USE_STARTUPHOOK
+#define USE_SHUTDOWNHOOK
+
+ /****** Object TASK ******/
+
+ /****** Object COUNTER ******/
+
+ /****** Object ALARM ******/
+
+ /****** Object RESOURCE ******/
+
+ /****** Object EVENT ******/
+
+ /****** Object ISR ******/
+
+ /****** Object APPMODE ******/
+
+#define appmode1	(AppModeType)( 1U << 0 )
+
+#endif	/* ! KERNEL_ID_H */
+
